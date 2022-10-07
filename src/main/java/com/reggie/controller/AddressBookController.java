e02347ae55cfa3ae093a4fd7985134d4ffcfece2
xmk <2549131895@qq.com>
2022-10-07 10:50:58 +0800

    手写了AddressBookController

--- src/main/java/com/reggie/controller/AddressBookController.java
+++ src/main/java/com/reggie/controller/AddressBookController.java
@@ -43,64 +43,27 @@ public class AddressBookController {
     }
 
     /**
-     *    设置默认地址
+     * 设置默认地址
      * @param addressBook
      * @return
      */
-    @PutMapping("/default")
+    @PutMapping("default")
     public R<AddressBook> setDefault(@RequestBody AddressBook addressBook){
         log.info("addressBook:{}",addressBook);
         LambdaUpdateWrapper<AddressBook> wrapper = new LambdaUpdateWrapper<>();
         wrapper.eq(AddressBook::getUserId,BaseContext.getCurrentId());
         wrapper.set(AddressBook::getIsDefault,0);
-        //Sql:update address_book set is_default = 0 where usser_id=?
+        //SQL:update address_book set is_default =0 where user_id=?
         addressBookService.update(wrapper);
 
         addressBook.setIsDefault(1);
-        //Sql:update address_book set is_default = 1 where id=?
+        //SQL:update address_book set is_default = 1 where id=?
         addressBookService.updateById(addressBook);
         return R.success(addressBook);
     }
 
     /**
-     * 根据id查地址
-     * @param id
-     * @return
-     */
-    @GetMapping("/{id}")
-    public R get(@PathVariable Long id){
-        AddressBook addressBook = addressBookService.getById(id);
-        if (addressBook != null){
-            return R.success(addressBook);
-        }else {
-            return R.error("没有找到该对象");
-        }
-    }
-
-    /**
-     * 查询默认地址
-     * @return
-     */
-    @GetMapping("default")
-    public R<AddressBook> getDefault(){
-        LambdaQueryWrapper<AddressBook> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(AddressBook::getUserId,BaseContext.getCurrentId());
-        queryWrapper.eq(AddressBook::getIsDefault,1);
-
-        //Sql:select * from address_book where user_id = ? and is_default = 1
-        AddressBook addressBook = addressBookService.getOne(queryWrapper);
-
-        if (addressBook == null){
-            return R.error("没有找到该对象");
-        }else{
-            return R.success(addressBook);
-        }
-    }
-
-    /**
      * 查询指定用户的全部地址
-     * @param addressBook
-     * @return
      */
     @GetMapping("/list")
     public R<List<AddressBook>> list(AddressBook addressBook){
@@ -109,10 +72,10 @@ public class AddressBookController {
 
         //条件构造器
         LambdaQueryWrapper<AddressBook> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(addressBook.getUserId() != null,AddressBook::getUserId,addressBook.getUserId());
+        queryWrapper.eq(null != addressBook.getUserId(),AddressBook::getUserId,addressBook.getUserId());
         queryWrapper.orderByDesc(AddressBook::getUpdateTime);
 
-        //Sql:select * from address_book where user_id = ? order by update_time desc
+        //SQL:select * from address_book where user_id = ? order by update_time desc
         return R.success(addressBookService.list(queryWrapper));
     }
 }
