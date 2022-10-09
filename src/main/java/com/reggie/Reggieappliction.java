0b0c0c32b388610e48913cd2f13feb96c43f9e66
xmk <2549131895@qq.com>
2022-10-09 22:40:34 +0800

    使用Spring Chche缓存套餐数据

--- src/main/java/com/reggie/Reggieappliction.java
+++ src/main/java/com/reggie/Reggieappliction.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -20,6 +21,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Slf4j
 @ServletComponentScan
 @EnableTransactionManagement
+@EnableCaching //开启Spring Cache注解方式的缓存功能
 public class Reggieappliction {
     public static void main(String[] args) {
         SpringApplication.run(Reggieappliction.class,args);
--- src/main/java/com/reggie/controller/SetmealController.java
+++ src/main/java/com/reggie/controller/SetmealController.java
@@ -12,6 +12,8 @@ import com.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -46,6 +48,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict( value = "setmealCache",allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info("套餐信息:{}",setmealDto);
 
@@ -108,6 +111,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict( value = "setmealCache",allEntries = true)
     public R<String> delete(@RequestParam List<Long> id){
         log.info("id:{}",id);
         setmealService.removeWithDish(id);
@@ -120,6 +124,7 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/list")
+    @Cacheable( value = "setmealCache",key = "#setmeal.categoryId + '_' + setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(setmeal.getCategoryId() != null,Setmeal::getCategoryId,setmeal.getCategoryId());
